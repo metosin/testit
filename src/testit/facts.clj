a1c193f2d22eeeffd73f165c721f8d020d0fcd8d
Jarppe <jarppe@metosin.fi>
2016-12-09 22:01:14 +0200

    Add support for expected part to generate test function, like `(fact resp => (content-type "text/plain"))`.

--- src/testit/facts.clj
+++ src/testit/facts.clj
@@ -18,15 +18,22 @@
        ~@(for [[value arrow expected] (partition 3 body)]
            `(fact ~value ~arrow ~expected)))))
 
+(defn- expected-fn? [body]
+  (or (and (seq? body)
+           (symbol? (first body)))
+      (and (seq? body)
+           (seq? (first body))
+           (symbol? (ffirst body)))))
+
 (declare =>)
 (defmethod assert-expr '=> [msg [_ & body]]
-  (assert-expr msg (concat (if-not (function? (first body))
-                             (list 'same))
-                           body)))
+  (assert-expr msg (if (expected-fn? body)
+                     body
+                     (cons 'same body))))
 
 (declare =not=>)
 (defmethod assert-expr '=not=> [msg [_ & body]]
-  (assert-expr msg (if (function? (first body))
-                     (conj (rest body)
-                           `(~'complement ~(first body)))
-                     (conj body 'not=))))
+  (assert-expr msg (if (expected-fn? body)
+                     (cons `not (list body))
+                     ; TODO: can't use 'same here
+                     (cons `not (list (cons `= body))))))
--- test/testit/facts_test.clj
+++ test/testit/facts_test.clj
@@ -2,29 +2,51 @@
   (:require [clojure.test :refer :all]
             [testit.facts :refer :all]))
 
+(defn close? [value margin]
+  (fn [result]
+    (< (- value margin)
+       result
+       (+ value margin))))
+
 (deftest clj-test-facts
 
   (testing "basics"
 
-    (fact ; "2 => 2"
-      2 => 2)
+    (fact "\"foo\" is string"
+      "foo" => string?)
 
-    (fact "value"
-      (+ 2 2) => 4)
+    ; Without name
+    (fact
+      "foo" => string?)
+
+    (fact
+      42 => 42)
+
+    (fact
+      (+ 40 2) => 42)
+
+    (fact
+      42 => pos?)
+
+    (fact
+      (conj [1 2] 3) => [1 2 3])
 
     (fact "function"
-      (+ 1 1) => pos?))
+      (+ 1 1) => pos?)
+
+    (fact "function generating test function"
+      40 => (close? 42 5))
+
+    ; TODO: It would be great if this would work too:
+    ; (fact
+    ;   40 => (+ 40 2)))
 
   (testing "=not=>"
-    (fact (+ 1 1) =not=> 1)
-    (fact (+ 1 1) =not=> neg?))
+    (fact
+      (+ 1 1) =not=> 1)
 
-  (testing "facts"
+    (fact
+      (+ 1 1) =not=> neg?)
 
-    (facts ;unnamed
-      1 => 1
-      1 => pos?)
-
-    (facts "more"
-      1 =not=> 0
-      1 =not=> neg?)))
+    (fact
+      1337 =not=> (close? 42 5)))))
