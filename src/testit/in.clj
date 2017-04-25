2e698f1cf3c67df621d2bc89640cad6a09fc9e63
Jarppe <jarppe@metosin.fi>
2017-04-25 21:29:27 +0300

    Added documentation for =in=>

--- src/testit/in.clj
+++ src/testit/in.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str])
   (:import (clojure.lang IExceptionInfo ILookup Associative Seqable)))
 
-(defn concatv [& colls]
+(defn- concatv [& colls]
   (vec (apply concat colls)))
 
 (defn deep-compare [path expected-form expected-value actual]
@@ -124,17 +124,41 @@
         :expected expected-form
         :actual actual}])))
 
+(defn- make-str [n c]
+  (apply str (repeat n c)))
+
+(defn equal-prefix-len [expected-value actual]
+  (loop [[e & more-e] expected-value
+         [a & more-a] actual
+         i 0]
+    (if (and e a (= e a))
+      (recur more-e more-a (inc i))
+      i)))
+
+(defn- diff-str [expected-value actual]
+  (let [equal-len (equal-prefix-len expected-value actual)
+        diff-len (- (count actual) equal-len)]
+    (str \space
+         (make-str equal-len \space)
+         (make-str diff-len \^))))
+
 (defn explain-errors [result]
   (->> result
        (remove (comp (partial = :pass) :type))
        (map (fn [{:keys [path message expected actual]}]
-              (format "%s\"%s\"\n-  expected: %s\n-  actual: %s"
+              (format (str "  %s  %s\n"
+                           "    expected: %s\n"
+                           "      actual: %s"
+                           (if (and (string? expected) (string? actual))
+                             "\n        diff: %s"))
                       (if (seq path)
-                        (str "in " (pr-str path) " ")
+                        (str "in " (pr-str path) ":\n")
                         "")
                       message
                       (pr-str expected)
-                      (pr-str actual))))
+                      (pr-str actual)
+                      (if (and (string? expected) (string? actual))
+                        (diff-str expected actual)))))
        (str/join "\n")))
 
 (defn generate-report [msg expected actual result]
