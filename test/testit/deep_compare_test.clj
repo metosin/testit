aaa6bcc7c065e517d93ed6017c2f0e3c08a46774
Jarppe <jarppe@metosin.fi>
2017-09-21 14:46:18 +0300

    Rewrote tests for new in/deep-compare.

--- test/testit/deep_compare_test.clj
+++ test/testit/deep_compare_test.clj
@@ -3,12 +3,68 @@
             [clojure.string :as str]
             [testit.core :refer :all]))
 
-(def deep-compare #'testit.core/deep-compare)
+(def deep-compare' #'testit.in/deep-compare)
+
+(defmacro deep-compare [expected actual]
+  `(deep-compare' [] (quote ~expected) ~expected ~actual))
 
 (deftest basics-tests
-  (facts
-    (deep-compare {:a 1} {:a 1}) => truthy
-    (deep-compare {:a 1} {:a 2}) => falsey
-    (deep-compare {:a {:b {:c 1, :d 2}}} {:a {:b {:c 1, :d 2}}}) => truthy
-    (deep-compare {:a {:b {:c 1, :d 2}}} {:a {:b {:c 1}}}) => falsey))
-
+  (fact
+    (deep-compare {:a 1} {:a 1}) => [{:path [:a]
+                                      :type :pass
+                                      :message "(= 1 1) => true"
+                                      :expected 1
+                                      :actual 1}])
+  (fact
+    (deep-compare {:a 1} {:a 2}) => [{:path [:a]
+                                      :type :fail
+                                      :message "(= 1 2) => false"
+                                      :expected 1
+                                      :actual 2}])
+  (fact
+    (deep-compare {:a {:b {:c 1, :d 2}}}
+                  {:a {:b {:c 1, :d 2}}})
+    => [{:path [:a :b :c]
+         :type :pass
+         :message "(= 1 1) => true"
+         :expected 1
+         :actual 1}
+        {:path [:a :b :d]
+         :type :pass
+         :message "(= 2 2) => true"
+         :expected 2
+         :actual 2}])
+  (fact
+    (deep-compare {:a {:b {:c 1, :d 2}}}
+                  {:a {:b {:c 1, :d 3}}})
+    => [{:path [:a :b :c]
+         :type :pass
+         :message "(= 1 1) => true"
+         :expected 1
+         :actual 1}
+        {:path [:a :b :d]
+         :type :fail
+         :message "(= 2 3) => false"
+         :expected 2
+         :actual 3}])
+  (fact
+    (deep-compare {:a {:b {:c 1, :d 2}}}
+                  {:a {:b {:c 1}}})
+    => [{:path [:a :b :c]
+         :type :pass
+         :message "(= 1 1) => true"
+         :expected 1
+         :actual 1}
+        {:path [:a :b :d]
+         :type :fail
+         :message "actual is missing key :d"
+         :expected 2
+         :actual nil}])
+  (fact
+    (deep-compare {:a {:b {:c 1}}}
+                  {:a {:b {:c 1, :d 2}}})
+    => [{:path [:a :b :c]
+         :type :pass
+         :message "(= 1 1) => true"
+         :expected 1
+         :actual 1}]))
