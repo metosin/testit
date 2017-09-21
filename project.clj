ef6655d94d32e3cd29a1e2c52b52e1be67b7d631
Jarppe <jarppe@metosin.fi>
2017-09-21 18:44:36 +0300

    Version to 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/testit "0.2.0"
+(defproject metosin/testit "0.2.1-SNAPSHOT"
   :description "Midje style assertions for clojure.test"
   :license {:name "Eclipse Public License", :url "http://www.eclipse.org/legal/epl-v10.html"}
 
